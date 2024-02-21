7639816f3175c5c03b41f7970afe6ab4c5cff47d
Fred-Abongo <136065099+Fred-Abongo@users.noreply.github.com>
2024-02-21 18:09:52 +0300

    scatter_squares.py

---
+++ Generating_Data/scatter_squares.py
@@ -0,0 +1,49 @@
+# Plotting and Styling Individual Points with scatter()
+
+import matplotlib.pyplot as plt
+
+# Scatter plot with a single point at coordinates (2, 4) and a point size of 200
+plt.scatter(2, 4, s=200)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Display the plot
+plt.show()# Plotting and Styling Individual Points with scatter()
+
+import matplotlib.pyplot as plt
+
+# Scatter plot with a single point at coordinates (2, 4) and a point size of 200
+plt.scatter(2, 4, s=200)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Display the plot
+plt.show()# Plotting and Styling Individual Points with scatter()
+
+import matplotlib.pyplot as plt
+
+# Scatter plot with a single point at coordinates (2, 4) and a point size of 200
+plt.scatter(2, 4, s=200)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Display the plot
+plt.show()
